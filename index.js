31969b627db1814cd51958adce923164510a0b95
Denis <kirugahdennis@gmail.com>
2026-05-07 11:49:57 +0300

    Combined User Functions

--- index.js
+++ index.js
@@ -4,3 +4,18 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+require("datejs");
+
+function combineUsers (...args){
+  let combinedObjects= {
+    users:[],
+    merge_date:Date.today().toString("M/D/YYYY")
+  };
+
+    for(let array of args){
+      combinedObjects.users.push(...array)
+    }
+
+    return combinedObjects;
+  }
